d8b30baa51c710de1a6b1749df87fc4ab035d7de
alumno <alumno@Alumno10>
2022-01-13 18:27:23 +0100

    GameController, GameDao, GameServiceImp -> update their content

--- LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
@@ -1,6 +1,14 @@
 package com.example.LucaSteam2.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 public class GameController {
-
+	
+	   //Guardar Usuario
+	   @PostMapping("/save")
+	   public String saveGame(Game game) {
+		    service.save(game);
+			return ("redirect:/");
+		}
 }
--- LucaSteam2/src/main/java/com/example/LucaSteam2/repository/GameDao.java
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/repository/GameDao.java
@@ -1,5 +1,20 @@
 package com.example.LucaSteam2.repository;
 
-public interface GameDao {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ejemplos.spring.model.User;
+
+@Repository
+public interface GameDao extends JpaRepository<Game, Integer>{
+	
+	List<Game> findByUsername(String name);
+	
+	// La Query anterior sería
+	// @Query("FROM Games WHERE name =?1")
+	@Query("FROM Game WHERE name =?1")
+	List<Game> searchAndFindByName(String name);
 }
