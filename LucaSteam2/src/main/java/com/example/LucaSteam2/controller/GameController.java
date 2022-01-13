da32331ed253373fc21bfdc2ea113d0ab8246d11
alumno <alumno@Alumno18>
2022-01-13 18:48:23 +0100

    addNewGame y controller

--- LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
@@ -1,14 +1,26 @@
 package com.example.LucaSteam2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.LucaSteam2.model.Game;
 
+@Controller
 public class GameController {
-	
-	   //Guardar Usuario
+		
+	   //Guardar Juego
 	   @PostMapping("/save")
 	   public String saveGame(Game game) {
 		    service.save(game);
 			return ("redirect:/");
 		}
+	   
+	   //Dar de alta un juego Nuevo
+	   @GetMapping("/newGame")
+	   public String addNewGame(Game game, Model model) {
+		   model.addAttribute("game", game);
+		   return "GameForm";
+	   }
 }
