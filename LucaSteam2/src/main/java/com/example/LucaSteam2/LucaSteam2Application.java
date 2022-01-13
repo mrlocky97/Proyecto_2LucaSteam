38116f4e4680cd1bbe1bdc7ad3705eab18866e29
alumno <alumno@Alumno14>
2022-01-13 17:02:27 +0100

    primer

---
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/LucaSteam2Application.java
@@ -0,0 +1,13 @@
+package com.example.LucaSteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaSteam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaSteam2Application.class, args);
+	}
+
+}
---
+++ LucaSteam2/src/test/java/com/example/LucaSteam2/LucaSteam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LucaSteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaSteam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
