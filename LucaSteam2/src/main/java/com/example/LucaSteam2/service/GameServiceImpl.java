5ac845120ede820f21e7e4ea259dce7b5ff06b17
alumno <alumno@Alumno07>
2022-01-14 10:25:53 +0100

    @Service y aplication. properties

--- LucaSteam2/src/main/java/com/example/LucaSteam2/service/GameServiceImpl.java
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/service/GameServiceImpl.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.LucaSteam2.model.Game;
 import com.example.LucaSteam2.repository.GameDao;
-
+@Service
 public class GameServiceImpl implements GameService{
 
 	@Autowired
