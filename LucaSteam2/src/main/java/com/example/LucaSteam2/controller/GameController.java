6f83be97242ee92ed8b667c2d01d155909940dc8
alumno <alumno@Alumno14>
2022-01-17 19:06:37 +0100

    estilos

--- LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
+++ LucaSteam2/src/main/java/com/example/LucaSteam2/controller/GameController.java
@@ -16,69 +16,75 @@ import com.example.LucaSteam2.service.GameService;
 public class GameController {
 	@Autowired
 	GameService service;
-		
-	   //Guardar Juego
-	   @PostMapping("/save")
-	   public String saveGame(Game game) {
-		    service.save(game);
-			return ("redirect:/");
-		}
-	   
-	   //Dar de alta un juego Nuevo
-	   @GetMapping("/newGame")
-	   public String addNewGame(Game game, Model model) {
-		   model.addAttribute("game", game);
-		   return "GameForm";
-	   }
-	   
-	// Editar juego
-		@GetMapping("/edit")
-		public String editGame(@RequestParam("id") int id, Model m) {
-			// Cuidado porque puede ser nulo. Debo decidir que hacer
-			m.addAttribute("game", service.findById(id));
-			return "GameForm";
-		}
 
-		// Listar juegos
-		@GetMapping("/")
-		public String listGames(Model m) {
-			m.addAttribute("gameList", service.findAll());
-			return "GameList";
-		}
-		
-		//Eliminar juego
-		@GetMapping("/delete")
-		public String deleteGame(@RequestParam("id") int id) {
-			service.deleteById(id);
-			return("redirect:/");
-		}
-		
-		//Encontrar por año
-		@GetMapping("/listyear")
-		public String yearList(@RequestParam("year") String year, Model m) {
-			
-			m.addAttribute("gameList", service.yearList(year));
-			
-			return "GameList";
-		}
-		
-		//Encontrar por género
-		@GetMapping("/listgenre")
-		public String listByGenre(@RequestParam("genre") String genre, Model m) {
-			m.addAttribute("gameList", service.listByGenre(genre));
-			return "GameList";
-		}
-		
-    //Encontrar lista por nintendo
-		@GetMapping("/findByNintendo")
-		public String nintendoList(Model model) {
-			model.addAttribute("gameList", service.findAllNintendo());
-			return("GameList");
-		}
-		
-		@RequestMapping("/test_hello")
-		public @ResponseBody String helloWorld() {
-			return "Hola Mundo";
-		}
+	// Guardar Juego
+	@PostMapping("/save")
+	public String saveGame(Game game) {
+		service.save(game);
+		return ("redirect:/list");
+	}
+
+	// Dar de alta un juego Nuevo
+	@GetMapping("/newGame")
+	public String addNewGame(Game game, Model model) {
+		model.addAttribute("game", game);
+		return "GameForm";
+	}
+
+	// Editar juego
+	@GetMapping("/edit")
+	public String editGame(@RequestParam("id") int id, Model m) {
+		// Cuidado porque puede ser nulo. Debo decidir que hacer
+		m.addAttribute("game", service.findById(id));
+		return "GameForm";
+	}
+
+	// Listar juegos
+	@GetMapping("/list")
+	public String listGames(Model m) {
+		m.addAttribute("gameList", service.findAll());
+		return "GameList";
+	}
+
+	// Listar juegos
+	@GetMapping("/")
+	public String menu(Model m) {
+		return "index";
+	}
+
+	// Eliminar juego
+	@GetMapping("/delete")
+	public String deleteGame(@RequestParam("id") int id) {
+		service.deleteById(id);
+		return ("redirect:/list");
+	}
+
+	// Encontrar por año
+	@GetMapping("/listyear")
+	public String yearList(@RequestParam("year") String year, Model m) {
+
+		m.addAttribute("gameList", service.yearList(year));
+
+		return "GameList";
+	}
+
+	// Encontrar por género
+	@GetMapping("/listgenre")
+	public String listByGenre(@RequestParam("genre") String genre, Model m) {
+		m.addAttribute("gameList", service.listByGenre(genre));
+		return "GameList";
+	}
+
+	// Encontrar lista por nintendo
+	@GetMapping("/findByNintendo")
+	public String nintendoList(Model model) {
+		model.addAttribute("gameList", service.findAllNintendo());
+		return ("GameList");
+	}
+
+	@RequestMapping("/test_hello")
+	public @ResponseBody String helloWorld() {
+		return "Hola Mundo";
+	}
 
 }
